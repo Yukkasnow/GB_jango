eb70f370bc6c359f41917734d839c4036ccb6fb8
Yukkasnow <93489035+Yukkasnow@users.noreply.github.com>
2022-10-06 15:38:17 +0300

    Lesson 4 (#4)
    * lesson_4 Models
    
    * lesson_4 Fixtures
    
    * lesson_4 Data migration
    
    * lesson_4 Display model's data

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "markdownify.apps.MarkdownifyConfig",
     "mainapp",
 ]
 
@@ -78,8 +79,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "gb_django",
+        "USER": "yukka",
+        "PASSWORD": "1234",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
